6e433d8cd436db20869837de514f9018a4017e8b
JuliaRDavid <juliaadavid@gmail.com>
2024-04-23 15:17:47 -0700

    Started mapPixelToCoords

--- Mandelbrot/ComplexPlane.cpp
+++ Mandelbrot/ComplexPlane.cpp
@@ -30,8 +30,7 @@ void ComplexPlane::zoomOut()
 }
 void ComplexPlane::setCenter(Vector2i mousePixel)
 {
-
-
+	m_plane_center = mapPixelToCoords(mousePixel);
 	m_state = State::CALCULATING;
 }
 void ComplexPlane::setMouseLocation(Vector2i mousePixel)
@@ -73,7 +72,7 @@ void ComplexPlane::updateRender()
 	}
 }
 
-int countIterations(Vector2f coord)
+int ComplexPlane::countIterations(Vector2f coord)
 {
 	complex<double> c(coord.x, coord.y);
 	complex<double> z(0, 0);
@@ -87,12 +86,14 @@ int countIterations(Vector2f coord)
 	return count;
 }
 
-void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
 
 }
 
-Vector2f mapPixelToCoords(Vector2i mousePixel)
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
 {
-	return Vector2f(0, 0);
+	Vector2f coords = { ((float(mousePixel.x) / m_pixel_size.x) * m_plane_size.x - (m_plane_center.x - m_plane_size.x)),
+							((float(mousePixel.y) / m_pixel_size.y) * m_plane_size.y - (m_plane_center.y - m_plane_size.y)) };
+	return coords;
 }
